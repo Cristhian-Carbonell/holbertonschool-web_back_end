5b6dc5b749dd23f015dcbed228fb9a0ed4aae33b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-07-28 14:05:04 -0500

    Simple pagination

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -10,30 +10,10 @@ Functions:
     get_page(object, integer, integer) -> list(list)
     index_range(integer, integer) -> tuple(integer, integer)
 """
-from typing import Tuple
+index_range = __import__('0-simple_helper_function').index_range
 import csv
 import math
 from typing import List
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    function named index_range that takes two integer arguments page
-    and page_size
-
-    Parameters:
-        page (int): index start number
-        page_size (int): index end number
-
-    Returns:
-        return a tuple of size two containing a start index and an end index
-        corresponding to the range of indexes
-    """
-    size_tuple_two = ()
-    start_index = page
-    end_index = page_size
-    size_tuple_two = ((end_index * (start_index - 1)), end_index * page)
-    return size_tuple_two
 
 
 class Server:
@@ -56,22 +36,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        NUM_DATASET = 19419
+        """Return page of dataset
+        """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
         page, page_size = index_range(page, page_size)
-        with open("file.csv") as csvfile:
-            if page > NUM_DATASET or page_size > NUM_DATASET:
-                return list()
-            datareader = csv.reader(csvfile)
-            dataset = []
-            line_count = 0 - 1
-            for row in datareader:
-                if line_count < page:
-                    line_count += 1
-                    continue
-                if page == page_size:
-                    break
-                dataset.append(row)
-                page += 1
-        return dataset
+        try:
+            return self.dataset()[page:page_size]
+        except IndexError:
+            return []
