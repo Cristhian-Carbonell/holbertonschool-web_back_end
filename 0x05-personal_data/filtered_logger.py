3cfd13567be56a7d77a840d1bbb086305f2b5043
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-02 18:45:22 -0500

    Regex-ing

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """Regex-ing
+
+Functions:
+    filter_datum(list(str), str, str, str) -> str:
 """
-from typing import List
+from typing import List, Pattern
 import re
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """[summary]
+    """returns the log message obfuscated
     """
     for index in fields:
         message = re.sub(index + "=.*?" + separator,
