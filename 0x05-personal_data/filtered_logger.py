bf5ab9417ad1d107851a908458867e2db2144921
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-03 11:39:13 -0500

    Log formatter

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ Functions:
 """
 from typing import List, Pattern
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -16,3 +17,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(index + "=.*?" + separator,
                          index + "=" + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
