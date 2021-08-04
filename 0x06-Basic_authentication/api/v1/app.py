ec5d0341683ddbc7637881fca68229c37951391b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-04 17:24:00 -0500

    Error handler: Forbidden

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -27,6 +27,7 @@ def error_unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def error_forbidden(error) -> str:
     """ error handler unauthorized
