a55ead7a242027e6f09ba996225b120d05d2d063
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-12 18:17:43 -0500

    Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -60,7 +60,8 @@ def before_request() -> str:
     if auth is not None:
         if not auth.require_auth(request.path, url_path):
             return
-        if auth.authorization_header(request) and auth.session_cookie(request):
+        if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
