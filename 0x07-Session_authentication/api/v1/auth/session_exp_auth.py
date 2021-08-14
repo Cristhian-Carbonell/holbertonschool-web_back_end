41bf892853150f3204075e5c5b5da6fd197e90ef
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-14 11:14:16 -0500

    Expiration?

---
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Module of  expiration date to a Session ID.
+"""
+from api.v1.auth.session_auth import SessionAuth
+from os import getenv
+from datetime import datetime, timedelta
+
+
+class SessionExpAuth(SessionAuth):
+    """class SessionAuth
+    """
+    def __init__(self):
+        """constructor method
+        """
+        self.session_duration = int(getenv("SESSION_DURATION"))
+        if not self.session_duration or not \
+                isinstance(self.session_duration, int):
+            self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """create session method
+        """
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        session_dictionary = self.user_id_by_session_id[session_id]
+        session_dictionary = {
+            'user_id': user_id,
+            'created_at': datetime.now()
+        }
+
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """user id for session id method
+        """
+        if session_id is None:
+            return None
+        if "session_id" not in self.user_id_by_session_id:
+            return None
+        if self.session_duration == 0:
+            return self.session_dictionary[session_id]["user_id"]
+        if "created_at" not in self.session_dictionary:
+            return None
+        if timedelta(self.created_at, self.session_duration) < datetime.now():
+            return None
+
+        return self.session_dictionary[session_id]["user_id"]
