796784cefe0010dfec67f9e13d9549165274bc71
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-19 16:01:45 -0500

    Hash password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -9,6 +9,5 @@ def _hash_password(password: str):
     """returns bytes.
     """
     encoded = password.encode('utf-8')
-    bytes = base64.b64encode(encoded)
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(bytes, salt)
+    return bcrypt.hashpw(encoded, salt)
