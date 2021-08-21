dd37572740fbd903701d3b267f4c7edd2245cd2e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-20 19:10:36 -0500

    Log out

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -55,6 +55,8 @@ def logout():
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
     destroy_session = AUTH.destroy_session(user.id)
     response = requests.get("http://10.42.0.181:5000/")
 
