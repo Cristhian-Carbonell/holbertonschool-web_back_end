eea10db55f16a79aed017f808db734b27235757f
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-08-31 21:18:06 -0500

    Storing lists

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -22,6 +22,22 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """call history function decorate
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper for decorator functionality
+        """
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+
+        return output
+    return wrapper
+
+
 class Cache:
     """Cache class
     """
@@ -32,6 +48,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store method
         """
