b38138b43f9883a18b0f10465cef097929d73762
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-12 23:51:50 -0500

    For...of Loops

--- 0x0E-ES6_basic/9-getFullBudget.js
+++ 0x0E-ES6_basic/9-getFullBudget.js
@@ -5,7 +5,10 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
-      return `$${income}`;
+    	return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+    	return `${income} euros`;
     },
   };
 
