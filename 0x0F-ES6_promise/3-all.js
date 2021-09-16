312a138ef7c5da06fb306201a818a6ff8b2453ed
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-16 10:36:43 -0500

    task 3 Handle multiple successful promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup() {
   const promises = [promisePhoto, promiseUser];
   return Promise.all(promises)
     .then((results) => {
-      console.log(results[0].body, results[1].firstName, results[1].lastName);
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
