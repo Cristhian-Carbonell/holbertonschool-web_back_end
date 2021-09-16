d4e7142ce3b27dba7b271f3cf722853b9caf3ded
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-16 16:50:54 -0500

    task 8 Throw error / try catch

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,10 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    try {
-      throw new Error('cannot divide by 0')
-    } catch (error) {
-      return error
-    }
+    throw new Error('cannot divide by 0')
   } else {
     return numerator / denominator
   }
