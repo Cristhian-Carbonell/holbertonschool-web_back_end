79fd4c51e68ff3b8d60bb0b8f21dc94dfc3bb1e1
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-15 22:57:00 -0500

    task 2 Catch me if you can!

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(ok => {
-    return {
+  return promise
+    .then(() => ({
       status: 200,
       body: 'success',
-    };
-  }).catch(() => { });
-  console.log('Got a response from the API');
-  console.log(promise);
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
