1e0f18e5673db306945bf06421cb88ff002b28ca
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-23 10:57:44 -0500

    task 11. Weak link data structure

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -2,12 +2,12 @@ const weakMap = new WeakMap();
 
 function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
-
+  const number = weakMap.set(endpoint, count + 1);
   if (weakMap.get(endpoint) >= 5) {
     throw Error('Endpoint load is high');
   }
 
-  return weakMap.set(endpoint, count + 1);
+  return number;
 }
 
 export { queryAPI, weakMap };
