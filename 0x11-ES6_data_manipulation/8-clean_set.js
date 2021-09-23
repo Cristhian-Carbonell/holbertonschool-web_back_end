291d6ef734a0ae70f25a8e34e7e176f45dd38371
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-22 22:48:23 -0500

    task 8. Clean set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,8 @@
 export default function cleanSet(set, startString) {
   const word = [];
-  if (startString.length === 0) {
+  if (typeof set !== 'object'
+  || typeof startString !== 'string'
+  || startString.length === 0) {
     return '';
   }
   for (const iterator of set) {
