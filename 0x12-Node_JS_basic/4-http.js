277954e1c78ac3d7f07b19147ba7d39bb8bc3b06
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-27 22:05:33 -0500

    task 4. Create a small HTTP server using Node's HTTP module

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -3,6 +3,8 @@ const app = require('http');
 const port = 1245;
 
 app.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Conten-Type', 'text/plain');
   res.write('Hello Holberton School!');
   res.end();
 }).listen(port);
