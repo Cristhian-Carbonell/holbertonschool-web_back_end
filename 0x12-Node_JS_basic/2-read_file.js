b5bc86fc9792b06911412324d282d37e8904bf64
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-27 20:08:20 -0500

    task 2. Reading a file synchronously with Node JS

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  let content;
+  let readFile;
   try {
-    content = fs.readFileSync(path);
+    readFile = fs.readFileSync(path);
   } catch (e) {
     throw new Error('Cannot load the database');
   }
-  console.log(content)
-};
+  readFile = readFile.toString().split('\n');
+  let students = readFile.map((value) => value.split(','));
+  console.log(`Number of students: ${students.length - 1}`)
+  const obj = {};
+  for (const line in students) {
+    if (line !== 0) {
+      if (!obj[students[line][3]]) {
+        obj[students[line][3]] = [];
+      }
+      obj[students[line][3]].push(students[line][0]);
+    }
+  }
+  delete obj.field;
+  for (const key of Object.keys(obj)) {
+    console.log(`Number of students in ${key}: ${obj[key].length}. List: ${obj[key].join(', ')}`,);
+  }
+}
