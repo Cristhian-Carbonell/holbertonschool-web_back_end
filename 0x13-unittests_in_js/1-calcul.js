9dcc56456afb3b5c1d27dc5c5f231b43cd25d485
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-29 22:09:45 -0500

    1. Combining descriptions

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,8 +1,19 @@
 function calculateNumber(type, a, b) {
-    a = Math.round(a);
-    b = Math.round(b);
-    return a + b;
+  if (typeof type === 'string' && typeof a === 'number' && typeof b === 'number') {
+      a = Math.round(a);
+      b = Math.round(b);
+    if (type === 'SUM') {
+      return a + b;
+    } else if (type === 'SUBTRACT') {
+      return a - b;
+    } else if (type === 'DIVIDE') {
+      if (a === 0 || b === 0) {
+        return 'Error';
+      }
+      return a / b;
+    }
   }
+}
   
   module.exports = calculateNumber;
   
