b0b3788cc0af90d794dfb28075ea55af11cd8111
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-30 17:11:48 -0500

    3. Spies

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,11 +1,13 @@
 const sendPaymentRequestToApi = require('./3-payment');
-const assert = require('assert');
+const chai = require('chai');
+const expect = chai.expect;
 const Utils = require('./utils');
 const sinon = require('sinon');
 
 describe('sendPaymentRequestToApi', function() {
   it('validate the usage of the Utils function', function() {
-    const spy = sinon.spy(sendPaymentRequestToApi(100, 20));
-    const spyUtils = sinon.spy(Utils, 'calculateNumber');
+    sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20)
+    expect(Utils.calculateNumber.calledOnce).to.be.true;
   })
 })
