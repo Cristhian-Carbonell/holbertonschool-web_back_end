2144a9954c28df636260810e6785923853967b89
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-09-29 21:29:37 -0500

    0. Basic test with Mocha and Node assertion library

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -8,9 +8,4 @@ describe('calculateNumber', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  it('should round a and b and return the sum of it ', function() {
-    assert.strictEqual(calculateNumber('1', 3.7), 5);
-    assert.strictequal(calculateNumber(1, '3'), 4);
-    assert.strictequal(calculateNumber('1.2', '3.7'), 5);
-  });
 });
